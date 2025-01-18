66ec80f02429ea83d6fd2b287f1941bd735c2216
adham-omran <git@adham-omran.com>
2025-01-18 23:51:23 +0300

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "A Nix-flake-based Clojure development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+  outputs = { self, nixpkgs }:
+
+    let
+      javaVersion = 21;
+      overlays = [
+        (final: prev: rec {
+          jdk = prev."jdk${toString javaVersion}";
+          boot = prev.boot.override { inherit jdk; };
+          clojure = prev.clojure.override { inherit jdk; };
+          leiningen = prev.leiningen.override { inherit jdk; };
+        })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+      {
+        devShells = forEachSupportedSystem ({ pkgs }: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Clojure
+              clojure-lsp
+              clojure
+              babashka
+              jdk
+              cljfmt
+              # Node
+              nodejs_22
+            ];
+            LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+          };
+        });
+      };
+  nixConfig.bash-prompt = "(nix) \\w \\$ ";
+}
