f8e884d48e507f6fbf37beeb19f04e4e7174b102
adham-omran <git@adham-omran.com>
2025-01-18 22:50:15 +0300

    Init commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to adham-omran/ir
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/com/adham_omran/ir_test.clj
@@ -0,0 +1,7 @@
+(ns adham-omran.ir-test
+  (:require [clojure.test :refer :all]
+            [com.adham-omran.irn.ir :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
