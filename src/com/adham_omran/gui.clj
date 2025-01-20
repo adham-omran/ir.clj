998089727ab2cbbf502fa18d7d8114baea7c7f0d
adham-omran <git@adham-omran.com>
2025-01-20 19:27:29 +0300

    Replace `gui` with `rerenderer`

--- src/com/adham_omran/gui.clj
+++ src/com/adham_omran/gui.clj
@@ -1,17 +1,17 @@
 (ns com.adham-omran.gui
   (:require [cljfx.api :as fx]))
 
-(def gui
-  (delay
-    (fx/on-fx-thread
-      (fx/create-component
-       {:fx/type :stage
-        :showing true
-        :title "Cljfx example"
-        :width 300
-        :height 300
-        :scene {:fx/type :scene
-                :root {:fx/type :v-box
-                       :alignment :center
-                       :children [{:fx/type :label
-                                   :text "Hello world"}]}}}))))
+(def renderer
+  (fx/create-renderer))
+
+(defn root [{:keys [showing]}]
+  {:fx/type :stage
+   :showing showing
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :padding 50
+                  :children [{:fx/type :button
+                              :text "close"
+                              :on-action (fn [_]
+                                           (renderer {:fx/type root
+                                                      :showing false}))}]}}})
