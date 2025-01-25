2e6a744c2a36d08cd47f23e7adfcd74737e679d1
adham-omran <git@adham-omran.com>
2025-01-25 16:48:01 +0300

    Update `com.adham-omran.impl.protocols`

--- src/com/adham_omran/impl/protocols.clj
+++ src/com/adham_omran/impl/protocols.clj
@@ -1,4 +1,5 @@
 (ns com.adham-omran.impl.protocols
+  (:refer-clojure :exclude [read])
   (:require
    [clojure.java.browse :as browse]
    [honey.sql :as sql]))
@@ -23,7 +24,28 @@
   (read [this]
     (browse/browse-url (:link this))))
 
-(let [element (->WebElement "https://en.wikipedia.org/wiki/Expression_problem")]
-  (read element))
+(comment
+  ;; Web Element
+
+  (let [element (->WebElement "https://en.wikipedia.org/wiki/Expression_problem")]
+    (read element))
+
+  (let [element (->WebElement "https://www.google.com")]
+    (add element)))
+
+(defrecord TextElement [link]
+  Element
+  (add [this]
+    (->> {:insert-into [:collection]
+          :columns [:afactor :priority :interval :date :kind :content]
+          :values [[1.0
+                    1.0
+                    1.0
+                    (.getEpochSecond (java.time.Instant/now))
+                    "text"
+                    this]]}
+         sql/format))
+  (read [this]
+    nil))
 
 ;; (add temp-e)
